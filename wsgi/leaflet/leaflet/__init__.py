67f537a9e65e17e284518e82668889fdd48ac2fc
Joseph Rawson <umeboshi@bard>
2012-12-10 10:48:28 -0600

    more trumpet setup

--- wsgi/leaflet/leaflet/__init__.py
+++ wsgi/leaflet/leaflet/__init__.py
@@ -4,7 +4,18 @@ from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
 from trumpet.models.base import DBSession, Base
+from trumpet.models.usergroup import populate
+from trumpet.models.wiki import populate_wiki
+from trumpet.models.rssdata import populate_feeds
+from trumpet.models.base import initialize_sql
 
+from trumpet.config.base import basetemplate, configure_base_layout
+from trumpet.config.static import configure_static
+from trumpet.config.wiki import configure_wiki
+from trumpet.config.rssviewer import configure_rssviewer
+from trumpet.config.login import configure_login
+
+from trumpet.security import authn_policy, authz_policy
 
 dbhost = os.environ['OPENSHIFT_POSTGRESQL_DB_HOST']
 dbport = os.environ['OPENSHIFT_POSTGRESQL_DB_PORT']
@@ -23,9 +34,18 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
+    initialize_sql(engine, [populate,
+                            populate_wiki,
+                            populate_feeds])
     config = Configurator(settings=settings)
+    configure_static(config)
+    configure_base_layout(config)
+    
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.scan()
+    config.add_view('trumpet.views.main.MainViewer',
+                    route_name='home',
+                    renderer=basetemplate,
+                    layout='base')
     return config.make_wsgi_app()
 
